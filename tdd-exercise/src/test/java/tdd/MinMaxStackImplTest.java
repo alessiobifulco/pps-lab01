99d44ff326408423bc49ff29d79d4a8054a18871
Alessio Bifulco <alessio.bifulco@studio.unibo.it>
2026-02-24 14:08:19 +0100

    test: initial stack state

--- tdd-exercise/src/test/java/tdd/MinMaxStackImplTest.java
+++ tdd-exercise/src/test/java/tdd/MinMaxStackImplTest.java
@@ -1,12 +1,26 @@
 package tdd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MinMaxStackImplTest {
+
+    MinMaxStack stack;
+
+    @BeforeEach
+    void init(){
+        stack = new MinMaxStackImpl();
+    }
+
     @Test
-    public void todo() {
-        assertTrue(true);
+    void testInitialStackIsEmpty() {
+        assertTrue(stack.isEmpty());
+    }
+
+    @Test
+    void testInitialStackSizeIsZero() {
+        assertEquals(0, stack.size());
     }
 }
